20d7ac0c24a904c1061d57b48b752796b2c03290
duiyho <104788670+duiyho@users.noreply.github.com>
2023-07-20 11:59:06 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa9b35eeb9eae473e0d63d0ca36ea0fbf9f3e4167";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
